799aadbc6e5dbc86c43f82471c6b098dda56bae7
Victor Assis <victorpombo20@gmail.com>
2026-07-15 16:26:11 -0300

    style: update garfo image to use Pinca-removebg-preview.png (v9)

--- lib/products.ts
+++ lib/products.ts
@@ -81,7 +81,7 @@ export const PRODUCTS: Product[] = [
     ],
     aplicacao:
       "Actros, Atego, Axor, FH, FM, R, P, Cargo, Constellation e demais modelos de linha pesada.",
-    imagem: "/images/produto-garfos-nobg-v8.png",
+    imagem: "/images/produto-garfos-nobg-v9.png",
     icon: "GitFork",
   },
   {
