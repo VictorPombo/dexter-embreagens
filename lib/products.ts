660723a542a6f454fe78c38a970fb06716067156
Victor Assis <victorpombo20@gmail.com>
2026-07-15 15:16:07 -0300

    style: replace garfo with flood-filled version (v6)

--- lib/products.ts
+++ lib/products.ts
@@ -81,7 +81,7 @@ export const PRODUCTS: Product[] = [
     ],
     aplicacao:
       "Actros, Atego, Axor, FH, FM, R, P, Cargo, Constellation e demais modelos de linha pesada.",
-    imagem: "/images/produto-garfos-nobg-v5.png",
+    imagem: "/images/produto-garfos-nobg-v6.png",
     icon: "GitFork",
   },
   {
