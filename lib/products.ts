0e24ef3fc5e4f0350e5c29f28904fa524a153cda
Victor Assis <victorpombo20@gmail.com>
2026-07-15 15:07:20 -0300

    style: update garfo image to v4 to fix alpha matting hole

--- lib/products.ts
+++ lib/products.ts
@@ -81,7 +81,7 @@ export const PRODUCTS: Product[] = [
     ],
     aplicacao:
       "Actros, Atego, Axor, FH, FM, R, P, Cargo, Constellation e demais modelos de linha pesada.",
-    imagem: "/images/produto-garfos-nobg-v2.png",
+    imagem: "/images/produto-garfos-nobg-v4.png",
     icon: "GitFork",
   },
   {
